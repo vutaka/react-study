2c5bb12e79adcd971e83e29a1a5deadb58728908
vutaka <vutaka0120@gmail.com>
2018-11-17 16:07:35 +0900

    hello world

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>hello react</p>
           <a
             className="App-link"
             href="https://reactjs.org"
